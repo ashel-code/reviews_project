e46c294f1813c0899b1d358f39cfda56a52f77d7
Рожков Даниил <rozhkov.danik@gmail.com>
2023-03-31 11:53:08 +0300

    Добавлен парсинг имен пользователей

--- parse_script.py
+++ parse_script.py
@@ -1,11 +1,12 @@
 import time
+import os.path
 from selenium import webdriver
 from bs4 import BeautifulSoup
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.by import By
 
-URL = "https://yandex.ru/maps/213/moscow/?ll=37.589146%2C55.731010&mode=poi&poi%5Bpoint%5D=37.58914" \
-      "6%2C55.731010&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D153977379747&tab=reviews&z=17.32"
+URL = "https://yandex.ru/maps/213/moscow/?ll=37.589146%2C55.731010&mode=poi&poi%5Bpoint%5D=37." \
+      "589146%2C55.731010&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D153977379747&tab=reviews&z=17.32"
 FILE_PATH = "page-source.html"
 
 
@@ -41,9 +42,10 @@ class Prints:
             print(i + 1, date.text)
 
     @staticmethod
-    def print_all(all_reviews, all_dates, all_ratings):
+    def print_all(all_reviews, all_dates, all_ratings, all_names):
         for i in range(len(reviews)):
-            print(i + 1, " - ", all_dates[i].text, " - ", all_ratings[i], " - ", all_reviews[i].text)
+            print(i + 1, " - ", all_names[i].text, " - ", all_dates[i].text,
+                  " - ", all_ratings[i], " - ", all_reviews[i].text)
 
 
 def get_reviews(new_soup):     # парсинг всех отзывов
@@ -66,6 +68,14 @@ def get_title(new_soup):
     return title[0].text
 
 
+def get_names(new_soup):
+    all_names = new_soup.find_all('div', class_="business-review-view__author")
+    fixed_names = []
+    for name in all_names:
+        fixed_names.append(name.find_next('a'))
+    return fixed_names
+
+
 def get_ratings(new_soup):
     items = new_soup.find_all('div', class_="business-rating-badge-view__stars")[1:]
     rates = []
@@ -79,8 +89,8 @@ def get_ratings(new_soup):
     return fixed_rates
 
 
-# TODO: check if file already exist`s, otherwise execute the line right down
-# Setup.get_source_html(URL)    DO IF IT IS THE FIRST RUN OF THE PROGRAM
+if os.path.exists(FILE_PATH) is False:
+    Setup.get_source_html(URL)
 soup = Setup.create_soup(FILE_PATH)
 
 
@@ -88,8 +98,9 @@ reviews = get_reviews(soup)
 total_mark = get_average_rating(soup)
 review_dates = get_dates(soup)
 rating = get_ratings(soup)
+names = get_names(soup)
 
 print("Название -", get_title(soup))
 print("Оценка -", total_mark[0].text + "." + total_mark[2].text)
 print("-------------------------------------------------------------------------")
-Prints.print_all(reviews, review_dates, rating)
+Prints.print_all(reviews, review_dates, rating, names)
