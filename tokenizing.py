86092b155f5ab49daa59c73fbb84a8b057da3975
f1rA5h <shtey.alexey@gmail.com>
2023-04-01 22:31:52 +0300

    visualization

--- tokenizing.py
+++ tokenizing.py
@@ -1,21 +1,47 @@
-# Импортируем библиотеки
-import gensim
-from gensim.models import Word2Vec
-import nltk
-nltk.download('punkt')
+from transformers import AutoTokenizer, AutoModel
+import torch
+import numpy as np 
 
-# Загружаем данные и проводим предобработку
-data = "Пример текста, который нужно перевести в векторную форму."
-sentences = nltk.sent_tokenize(data)
-word_tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
+# Загрузка предобученной модели и токенизатора
+tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
+model = AutoModel.from_pretrained("bert-base-multilingual-cased")
 
-# Обучаем модель Word2Vec на предобработанных данных
-model = Word2Vec(word_tokens, min_count=1)
+# Входной текст
+text = '''
+28 Ну, что ж! Очередное место в Москве с отличной локацией, отличным названием, отличной идеей и концепцией, НО хамским неадекватным персоналом, демонстративно держащим перед тобой какую-то папку с  мятой бумагой, с перечеркнутыми именами. И вот этот вот малообразованное провинциальное хамло, пытается изобразить из себя винный уютный ресторан. Качество еды и выбор вина отвратительный! Никому не советую это место, хотя чисто из-за расположения и отсутствия конкуренции, клиентов, видимо, хватает. Но, это совсем другая и только московская история!
+'''
 
-# Получаем вектор для каждого слова
-vectors = []
-for word in word_tokens:
-    vector = model.wv[word]
-    vectors.append(vector)
+# Токенизация текста
+tokens = tokenizer.encode(text, add_special_tokens=True)
 
-print(vectors)
+# Преобразование токенов в тензор
+tensor = torch.tensor([tokens])
+
+# Прохождение тензора через модель для получения вектора
+with torch.no_grad():
+    output = model(tensor)
+
+# Извлечение вектора из выходных данных модели
+vector = output[0][0]
+
+# Вывод вектора
+# Преобразование тензора в массив NumPy
+vector_np = vector.numpy()
+
+# Установка опции вывода массива
+np.set_printoptions(threshold=np.inf)
+
+# Вывод массива
+np.savetxt('vec.temp', vector_np)
+print("finished")
+
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+# Сокращение размерности вектора с помощью t-SNE
+tsne = TSNE(n_components=2, random_state=42)
+vector_tsne = tsne.fit_transform(vector_np)
+
+# Визуализация вектора
+plt.scatter(vector_tsne[:,0], vector_tsne[:,1])
+plt.show()
