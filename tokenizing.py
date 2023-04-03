aba1f0629ee2d1130d1388b6e220cdffe5a09bdd
f1rA5h <shtey.alexey@gmail.com>
2023-04-03 14:10:16 +0300

    fixed bug with 14884 comments

--- tokenizing.py
+++ tokenizing.py
@@ -3,13 +3,14 @@ from transformers import BertModel, BertTokenizer
 import matplotlib.pyplot as plt
 
 def tok(texts):
+    for i in range(len(texts)):
+        print(i, texts[i])
     # Загрузка модели BERT и токенизатора
     model = BertModel.from_pretrained('bert-base-multilingual-cased')
     tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 
     # Список текстов для обработки
     
-    print(texts)
 
     # Токенизация и пакетирование текстов
     tokenized_texts = [tokenizer.encode(text, add_special_tokens=True) for text in texts]
@@ -27,7 +28,13 @@ def tok(texts):
 
     # Визуализация векторов
     fig, ax = plt.subplots()
+    print(len(texts))
+
     for i, text in enumerate(texts):
+        # print(i)
         ax.scatter(last_hidden_states[i, 0], last_hidden_states[i, 1], label=i)
+        
+        ax.annotate(str(i), (last_hidden_states[i, 0], last_hidden_states[i, 1]))
+
     ax.legend()
     plt.show()
