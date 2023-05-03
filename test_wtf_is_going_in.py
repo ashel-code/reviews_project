b1d5db585ceb3d4663e7126870337bea6a7e143e
f1rA5h <shtey.alexey@gmail.com>
2023-05-03 21:50:46 +0300

    del trash

--- test_wtf_is_going_in.py
+++
@@ -1,6 +0,0 @@
-from mysql.connector import connect
-
-with connect(host="localhost", user="root", password="BackupDR") as connection:
-    db_request = 'CREATE DATABASE reviews'
-    with connection.cursor() as cursor:
-        cursor.execute(db_request)
