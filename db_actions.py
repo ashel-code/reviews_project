fc230509e889d7e3957f72cd79480f0d46acfd71
DanikRozhkov <rozhkov.danik@gmail.com>
2023-05-06 17:24:52 +0300

    Метод get_unvectorized(), который выдает все отзывы, у которых нет векторного представления

--- db_actions.py
+++ db_actions.py
@@ -1,4 +1,5 @@
 from mysql.connector import connect
+from structure.unvectorized_data import Unvectorized
 
 
 class DatabaseActions:
@@ -14,6 +15,32 @@ class DatabaseActions:
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 result = cursor.fetchall()
-                print(result)
                 cursor.close()
             connection.close()
+        return result
+
+    @staticmethod
+    def get_unvectorized():
+        with connect(
+                host="localhost",
+                user="root",
+                password="BackupDR",
+                database="reviews"
+        ) as connection:
+            query = 'SELECT id, review_text FROM reviews WHERE vector IS NULL'
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                pre_result = cursor.fetchall()
+                cursor.close()
+            connection.close()
+        print(pre_result)
+        result = DatabaseActions.convert_to_unvectorized_class(pre_result)
+        return result
+
+    @staticmethod
+    def convert_to_unvectorized_class(data):
+        result = []
+        for d in data:
+            temp = Unvectorized(d[0], d[1])
+            result.append(temp)
+        return result
