f3182af6912b39da77b72962f7e7b30ca6810e39
f1rA5h <shtey.alexey@gmail.com>
2023-06-12 09:24:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 
 @app.route('/process_list', methods=['POST'])
 def process_list():
-    print("~" * 80, '\n')
     data = request.json['list']
-    print("~" * 80, '\n', request.json['list'])
     # Assuming the input is a JSON object with a "list" key containing the list
     input_list = data
 
